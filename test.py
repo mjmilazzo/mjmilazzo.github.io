c8892ff4590f2574bd422b5cca67f237287d9352
mjmilazzo <41544812+mjmilazzo@users.noreply.github.com>
2021-02-23 21:15:59 -0800

    refactoring to process from markdown files
    adding layer to create webpages from markdown text files

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 
-x = pd.DataFrame({
-    'a': np.arange(10),
-    'b': range(10)
-})
+# x = pd.DataFrame({
+#     'a': np.arange(10),
+#     'b': range(10)
+# })
 
-print(x)
-print("hello")
+# print(x)
+# print("hello")
+
+import markdown
+
+with open("main page markdown.txt", "r", encoding="utf-8") as input_file:
+    text = input_file.read()
+
+html = markdown.markdown(text)
+
+# txt = """
+# #This is a markdown file
+# """
+# html = markdown.markdown(txt)
+# print(html)
+
+with open("index.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+    output_file.write(html)
